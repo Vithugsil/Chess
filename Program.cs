27123e9b32999cfbe4efea0e48a8e4a124284f06
vithugsil <vitorhugosilva012@gmail.com>
2024-02-05 06:35:14 -0300

    Adicionado Posicoes

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Tabuleiro;
+
+namespace Course
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Posicao P = new(3, 4);
+            System.Console.WriteLine(P.ToString());
+        }
+    }
+}
